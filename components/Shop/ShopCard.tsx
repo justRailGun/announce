b732b112870233bfbb6cba921fbc511bf8862815
justRailGun <juustjimmy17@gmail.com>
2024-11-07 13:21:55 +0100

    Finished cardShop components

--- components/Shop/ShopCard.tsx
+++ components/Shop/ShopCard.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Card, CardContent } from "@/components/ui/card"
-import { Star } from 'lucide-react'
 import Image from 'next/image'
 import { Shop } from '@/constants/Shop'
+import { Rating } from '@mui/material'
 
 
 const ShopCard = ({shop} : {shop: Shop}) => {
 
-    const {name,bannerURL,popularity,overallRating,numberOfOrders,location,phone,email,address} = shop
+    const {name,bannerURL,popularity,overallRating,numberOfOrders} = shop
   return (
-    <Card className="w-72 overflow-hidden transition-all mx-4 duration-300 hover:shadow-lg">
+    <Card className="w-72 shadow-lg overflow-hidden transition-all m-4 duration-300 hover:shadow-lg">
     <div className="relative h-48 overflow-hidden">
       <Image
         src={bannerURL}
@@ -19,19 +19,17 @@ const ShopCard = ({shop} : {shop: Shop}) => {
         className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
       />
     </div>
-    <CardContent className="p-4">
-      <h2 className="text-xl font-semibold mb-2 text-gray-800">{name}</h2>
-      <div className="flex justify-between items-center">
-        <span className="text-lg font-bold text-green-600">$129.99</span>
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <Star className="w-5 h-5 text-yellow-400 fill-current" />
-            <Star className="w-5 h-5 text-yellow-400 fill-current" />
-            <Star className="w-5 h-5 text-yellow-400 fill-current" />
-            <Star className="w-5 h-5 text-yellow-400 fill-current" />
-            <Star className="w-5 h-5 text-gray-300 fill-current" />
+    <CardContent className="p-4 invert-colors">
+      <h2 className="text-xl font-semibold  text-white">{name}</h2>
+      <div className="flex justify-between flex-col">
+        <span className="text-lg  font-bold text-blue-600 flex items-center gap-2">Popularity : {popularity} <Image src="/icons/users-group.svg" width={20} height={20} alt="star" /></span>
+        <div className="flex items-center justify-between">
+          <div className="flex justify-center items-center gap-2">
+          <Rating name="read-only" value={overallRating} readOnly precision={0.1}/>{overallRating}
           </div>
-          <span className="ml-2 text-sm text-gray-600">4.0 <span className="text-gray-400">(128)</span></span>
+          <span className="flex items-center gap-2 text-gray-600"> 
+            {numberOfOrders} <Image src="/icons/order.svg" width={20} height={20} alt="star" />
+          </span>
         </div>
       </div>
     </CardContent>
